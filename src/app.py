83bf25e05539070b96b754f5deb7dee157b724a9
Rohit <rohit@gmail.com>
2023-11-02 14:08:37 +0530

    Get to POST

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from sql_and_docs_chat import init_model
 app = flask.Flask(__name__)
 
 model = init_model()
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["POST"])
 def starting_url():
     json_data = flask.request.json
     user_input = json_data["user_input"]
